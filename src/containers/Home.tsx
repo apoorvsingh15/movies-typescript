e92bcf0da52a8c0406e8224a6d1582898f6ef963
Apoorv Singh <apoorv.singh15@gmail.com>
2022-08-04 23:26:39 +0530

    :zap: added home page - wip

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
-
+import { Button, Card } from 'antd';
+import '../css/Home.css';
 const Home = () => {
+    const { Meta } = Card;
     return (
-        <div>
-            <h1>Welcome to React!</h1>
+        <div className="mainClass">
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+                <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card>
         </div>
     );
 }
