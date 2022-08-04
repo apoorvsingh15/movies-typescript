b8adb29f731cc86c58ffa1caa706c18d84c472a0
Apoorv Singh <apoorv.singh15@gmail.com>
2022-08-04 20:16:32 +0530

    :zap: first commit

---
+++ src/api/config.ts
@@ -0,0 +1,2 @@
+export const BASE_URL: String = 'http://www.omdbapi.com/';
+export const API_KEY: Number = 439370e2;
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "../containers/Home";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes
