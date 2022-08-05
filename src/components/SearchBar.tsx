3da02a755e6f965b6e829b4eb5d101ec70104fdc
Apoorv Singh <apoorv.singh15@gmail.com>
2022-08-05 13:16:50 +0530

    :zap: added frontpage

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,3 @@
-import { AudioOutlined } from '@ant-design/icons';
 import { Input, Space } from 'antd';
 import React from 'react';
 import '../css/SearchBar.css';
@@ -9,7 +8,7 @@ const onSearch = (value: string) => console.log(value);
 
 const SearchBar: React.FC = () => (
     <Space className='search-container' direction="vertical" >
-        <Search placeholder="Search a movie..." onSearch={onSearch} style={{ width: 300 }} />
+        <Search placeholder="Search a movie..." onSearch={onSearch} style={{ width: 300 }} defaultValue="Batman" />
     </Space>
 );
 
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,20 +1,75 @@
-import * as React from "react";
-import { Button, Card } from 'antd';
+import React, { useState, useEffect, useRef } from "react";
+import { Button, Card, Col, Row } from 'antd';
 import '../css/Home.css';
 import SearchBar from "../components/SearchBar";
+import { getDefaultMovies } from "../api/getDefaultMovies";
+
+interface IMovieList {
+    data: {
+        Search: []
+    }
+}
+
+
 const Home = () => {
+    const [movies, setMovies] = useState<string[]>([]);
+    const [page, setPage] = useState<number>(1);
     const { Meta } = Card;
+
+    const getMoviesToDisplay = async () => {
+        try {
+            const movieList: IMovieList = await getDefaultMovies(page);
+            setMovies([...movies, ...movieList.data.Search])
+        } catch (err) {
+            console.log('Something went Wrong')
+        }
+    }
+
+    const onClickLoadMore = () => {
+        setPage((prev: number) => prev + 1)
+    }
+
+    useEffect(() => {
+        getMoviesToDisplay();
+    }, [page]);
+
+    console.log(movies)
+
+
     return (
         <div className="mainClass">
             <SearchBar />
-            <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-            >
-                <Meta title="Europe Street beat" description="www.instagram.com" />
-            </Card>
-        </div>
+            <Row gutter={16}>
+                {
+                    movies?.length && movies?.map((singleMovie: any) => (
+                        <Col
+                            className="gutter-row"
+                            span={6}
+                            key={singleMovie.imdbID}
+                        >
+                            <Card
+                                hoverable
+                                style={{ margin: '15px 10px 0px 10px' }}
+                                cover={<img alt={singleMovie.Title}
+                                    src={singleMovie.Poster === 'N/A' ?
+                                        'https://images.pexels.com/photos/12877900/pexels-photo-12877900.jpeg' : singleMovie.Poster} />}
+                            >
+                                <Meta
+                                    title={singleMovie.Title}
+                                    description={singleMovie.Year}
+                                />
+                            </Card>
+                        </Col>
+                    ))
+                }
+            </Row>
+            {movies?.length ? <div className="buttonContainer">
+                <Button
+                    type="primary"
+                    onClick={onClickLoadMore}
+                >Load More...</Button>
+            </div> : null}
+        </div >
     );
 }
 
